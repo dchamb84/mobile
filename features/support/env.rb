c383a253d7bc1d2798abe2b24019d87fbb727024
David Chambers <daa.chambers@gmail.com>
2013-01-11 14:38:19 +0000

    change the driver used

--- features/support/env.rb
+++ features/support/env.rb
@@ -7,14 +7,14 @@ require 'selenium-webdriver'
 
 #setup config for browser
 Capybara.configure do |config|
-  config.default_driver = :selenium_chrome
+  config.default_driver = :selenium
   config.run_server = false
   config.default_selector = :css
   config.default_wait_time = 10
 end
 
-Capybara.register_driver :selenium_chrome do |app|
-  Capybara::Selenium::Driver.new(app, :browser => :chrome)
+Capybara.register_driver :selenium do |app|
+  Capybara::Selenium::Driver.new(app, :browser => :firefox)
 end
 
 World(Capybara)
